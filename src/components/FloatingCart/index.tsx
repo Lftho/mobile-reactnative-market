557d865f5a8f2f46c276957d0edb94ed4a348b7c
Luiz Fernando Thomaz <lft.thomaz@gmail.com>
2021-01-27 21:58:17 -0300

    Desenvolvido toda funcionalidade do projeto

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,23 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products
+      .map(product => product.price * product.quantity)
+      .reduce((accumulator, current) => {
+        return accumulator + current;
+      }, 0);
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const ProductQuantity = products
+      .map(product => product.quantity)
+      .reduce((accumulator, current) => {
+        return accumulator + current;
+      }, 0);
 
-    return 0;
+    return ProductQuantity;
   }, [products]);
 
   return (
